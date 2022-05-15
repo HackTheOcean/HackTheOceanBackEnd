efeb658bc2e44d1404f1c8f73d9d950dd4ed4e37
pe-ca <mr.gloser1@gmail.com>
2022-05-15 10:33:18 -0500

    actualizadas pruebas para clase oceanWildlifeService

--- test/services/oceanWildlifeService.test.js
+++ test/services/oceanWildlifeService.test.js
@@ -3,19 +3,15 @@ const ows = require('./../../lib/services/oceanWildlifeService')
 describe("Suite test para oceanWildlifeService", ()=>{
     test("Verificacion de existencia de metodos de consulta", ()=>{
         const getall = typeof ows.getAll
-        const getone = typeof ows.getOne
         const getonedetails = typeof ows.getOneDetails
         expect(getall).toBe("function")
-        expect(getone).toBe("function")
         expect(getonedetails).toBe("function")
     })
 
     test("El resultado no debe ser indefinido", async ()=>{
         const getall = await ows.getAll()
-        const getone = await ows.getOne()
-        const getonedetails = await ows.getOneDetails()
+        const getonedetails = await ows.getOneDetails(1)
         expect(getall).not.toBeUndefined()
-        expect(getone).not.toBeUndefined()
         expect(getonedetails).not.toBeUndefined()
     })
 
@@ -23,11 +19,6 @@ describe("Suite test para oceanWildlifeService", ()=>{
         const result = await ows.getAll()
         const evaluation = Array.isArray(result)
         expect(evaluation).toBe(true)
-    })
-
-    test("getOne: debe regresar un json con la informacion correspondiente al id", async ()=>{
-        const result = typeof await ows.getOne()
-        expect(result).toBe("object")
     })
 
     test("getOneDetails: debe regresar un json con la informacion correspondiente al id", async ()=>{
