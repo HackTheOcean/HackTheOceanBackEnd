bb9a15a4ecdebc2dba37ecf739ea3424ab232901
pe-ca <mr.gloser1@gmail.com>
2022-05-15 11:21:37 -0500

    aplicadas reglas de estilo sobre todo el proyecto

--- test/services/oceanPollutionService.test.js
+++ test/services/oceanPollutionService.test.js
@@ -1,29 +1,29 @@
-const ops = require('../../lib/services/oceanPollutionService')
+const ops = require("../../lib/services/oceanPollutionService");
 
 describe("Suite test para oceanPollution", ()=>{
     test("Verificacion de existencia de metodos de consulta", ()=>{
-        const getall = typeof ops.getAll
-        const getonedetails = typeof ops.getOneDetails
-        expect(getall).toBe("function")
-        expect(getonedetails).toBe("function")
-    })
+        const getall = typeof ops.getAll;
+        const getonedetails = typeof ops.getOneDetails;
+        expect(getall).toBe("function");
+        expect(getonedetails).toBe("function");
+    });
 
     test("El resultado no debe ser indefinido", async ()=>{
-        const getall = await ops.getAll()
-        const getonedetails = await ops.getOneDetails()
-        expect(getall).not.toBeUndefined()
-        expect(getonedetails).not.toBeUndefined()
-    })
+        const getall = await ops.getAll();
+        const getonedetails = await ops.getOneDetails();
+        expect(getall).not.toBeUndefined();
+        expect(getonedetails).not.toBeUndefined();
+    });
 
     test("getAll: debe regresar un array con todos los datos", async ()=>{
-        const result = await ops.getAll()
-        const evaluation = Array.isArray(result)
-        expect(evaluation).toBe(true)
-    })
+        const result = await ops.getAll();
+        const evaluation = Array.isArray(result);
+        expect(evaluation).toBe(true);
+    });
 
     test("getOneDetails: debe regresar un json con la informacion correspondiente al id", async ()=>{
-        const result = typeof await ops.getOneDetails()
-        expect(result).toBe("object")
-    })
+        const result = typeof await ops.getOneDetails();
+        expect(result).toBe("object");
+    });
 
-})
+});
--- test/services/oceanWildlifeService.test.js
+++ test/services/oceanWildlifeService.test.js
@@ -1,29 +1,29 @@
-const ows = require('./../../lib/services/oceanWildlifeService')
+const ows = require("./../../lib/services/oceanWildlifeService");
 
 describe("Suite test para oceanWildlifeService", ()=>{
     test("Verificacion de existencia de metodos de consulta", ()=>{
-        const getall = typeof ows.getAll
-        const getonedetails = typeof ows.getOneDetails
-        expect(getall).toBe("function")
-        expect(getonedetails).toBe("function")
-    })
+        const getall = typeof ows.getAll;
+        const getonedetails = typeof ows.getOneDetails;
+        expect(getall).toBe("function");
+        expect(getonedetails).toBe("function");
+    });
 
     test("El resultado no debe ser indefinido", async ()=>{
-        const getall = await ows.getAll()
-        const getonedetails = await ows.getOneDetails(1)
-        expect(getall).not.toBeUndefined()
-        expect(getonedetails).not.toBeUndefined()
-    })
+        const getall = await ows.getAll();
+        const getonedetails = await ows.getOneDetails(1);
+        expect(getall).not.toBeUndefined();
+        expect(getonedetails).not.toBeUndefined();
+    });
 
     test("getAll: debe regresar un array", async ()=>{
-        const result = await ows.getAll()
-        const evaluation = Array.isArray(result)
-        expect(evaluation).toBe(true)
-    })
+        const result = await ows.getAll();
+        const evaluation = Array.isArray(result);
+        expect(evaluation).toBe(true);
+    });
 
     test("getOneDetails: debe regresar un json con la informacion correspondiente al id", async ()=>{
-        const result = typeof await ows.getOneDetails()
-        expect(result).toBe("object")
-    })
+        const result = typeof await ows.getOneDetails();
+        expect(result).toBe("object");
+    });
 
-})
+});
